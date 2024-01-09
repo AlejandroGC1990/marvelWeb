d5d9e5ba8c69afe853039ad5a7dd2079766f3a26
AlexOscar <alejandro.garcia.carmona@gmail.com>
2024-01-09 22:39:19 +0100

    HeroCard

--- marvel-web/app/page.tsx
+++ marvel-web/app/page.tsx
@@ -1,13 +1,18 @@
-import Image from 'next/image'
-import Navbar from './ui/Components/Navbar/Navbar'
+import Image from "next/image";
+import Navbar from "./ui/Components/Navbar/Navbar";
+import HeroCard from "./ui/Components/HeroCard/HeroCard";
+
 
 export default function Home() {
   return (
     <main>
-      <div className="w-full flex-none md:w-64">
+      <div className="app-container">
         <Navbar />
+        <h1>Home</h1>
+        <div>
+          <HeroCard />
+        </div>
       </div>
-      <h1>Home</h1>
     </main>
-  )
+  );
 }
