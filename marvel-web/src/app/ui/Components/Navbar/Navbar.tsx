35a3d7aedd1ded2c1d11a3420b47af8c71e07489
AlexOscar <alejandro.garcia.carmona@gmail.com>
2024-01-11 20:28:49 +0100

    button back to Home in navbar

--- marvel-web/src/app/ui/Components/Navbar/Navbar.tsx
+++ marvel-web/src/app/ui/Components/Navbar/Navbar.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import "./Navbar.css";
+import Link from "next/link";
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
   return (
     <div className="navbar-container">
-      <div className="navbar-title">Navbar - Marvel Gate</div>
+      <Link href="/" className="navbar-title">Navbar - Marvel Gate</Link>
       <div className="navbar-input">
         <input placeholder="Your super..." />
       </div>
